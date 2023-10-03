d4e8b2f4df1f1efff3b0d56866818141ad79d380
TrailrunnerDLH <129078484+TrailrunnerDLH@users.noreply.github.com>
2023-10-03 18:15:34 -0500

    Using D3 library to read JSON file from url

---
+++ static/js/app.js
@@ -0,0 +1,24 @@
+const samplesURL = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Use D3 library to read samples.JSON file 
+function init() {
+    
+    d3.json(samplesURL).then(function (data) {
+        
+       
+    })
+};
+
+init();
