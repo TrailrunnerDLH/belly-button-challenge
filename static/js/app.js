dc46799ba538a1228c1939e08d067745899bef68
TrailrunnerDLH <129078484+TrailrunnerDLH@users.noreply.github.com>
2023-10-03 18:36:57 -0500

    Dropdown menu created

--- static/js/app.js
+++ static/js/app.js
@@ -14,9 +14,13 @@ const samplesURL = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-cla
 
 // Use D3 library to read samples.JSON file 
 function init() {
-    
+    let dropDown = d3.select('#selDataset');
     d3.json(samplesURL).then(function (data) {
-        
+        let names = data.names;
+        let samples = data.samples;
+        Object.values(names).forEach(value => {
+            dropDown.append('option').text(value);
+        })
        
     })
 };
