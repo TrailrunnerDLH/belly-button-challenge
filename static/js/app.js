a02bda321657d8b1186ab3e742cd7d0c70c03532
TrailrunnerDLH <129078484+TrailrunnerDLH@users.noreply.github.com>
2023-10-03 19:04:48 -0500

    Created bar plot

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,29 @@
 const samplesURL = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
+let sampleData;
 
 
+//Create bar plot
+function barPlot(id) {
+    let samples = sampleData.samples;
+    let identifier = samples.filter(sample => sample.id === id);
+    let filtered = identifier[0];
+    let OTUvalues = filtered.sample_values.slice(0, 10).reverse();
+    let OTUids = filtered.otu_ids.slice(0, 10).reverse();
+    let labels = filtered.otu_labels.slice(0, 10).reverse();
+    let barTrace = {
+        x: OTUvalues,
+        y: OTUids.map(object => 'OTU ' + object),
+        name: labels,
+        type: 'bar',
+        orientation: 'h'
+    };
+ 
+   
+    let barData = [barTrace];
+    Plotly.newPlot('bar', barData);
 
+}
 
 
 
@@ -16,11 +37,13 @@ const samplesURL = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-cla
 function init() {
     let dropDown = d3.select('#selDataset');
     d3.json(samplesURL).then(function (data) {
-        let names = data.names;
-        let samples = data.samples;
+        sampleData = data
+        let names = sampleData.names;
+        let samples = sampleData.samples;
         Object.values(names).forEach(value => {
             dropDown.append('option').text(value);
         })
+        barPlot(names[0])
        
     })
 };
